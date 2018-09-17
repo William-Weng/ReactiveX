424b7036346d4054fb5771de3f47123b24f04d68
翁禹斌(William.Weng) <linuxice0609@gmail.com>
2018-09-17 13:50:06 +0800

    加入RxJS

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ReactiveX
 
 1. RxSwift
+	1. RxSwift_HelloWorld
+	1. RxSwift_TableView
+1. RxJS
+	1. HelloWorld - 按住滑鼠左鍵畫線
---
+++ RxJS/HelloWorld/main.js
@@ -0,0 +1,30 @@
+/// [Mouse Drag with RxJS](https://medium.com/@jdjuan/mouse-drag-with-rxjs-45861c4d0b7e)
+/// [RxJS - RxJS v5.x to v6 Update Guide](https://rxjs-dev.firebaseapp.com/guide/v6/migration#import-paths)
+/// [RxJS/fromevent.md at master · Reactive-Extensions/RxJS · GitHub](https://github.com/Reactive-Extensions/RxJS/blob/master/doc/api/core/operators/fromevent.md#rxobservablefromeventelement-eventname-selector-options)
+
+import { paint } from './canvas.js'
+
+const { fromEvent } = rxjs
+const { takeUntil, mergeMap } = rxjs.operators
+
+const move$ = fromEvent(document.body, 'mousemove')
+const down$ = fromEvent(document.body, 'mousedown')
+const up$ = fromEvent(document.body, 'mouseup')
+
+/// 按下滑鼠左鍵後，結合滑鼠的移動動作，直到滑鼠左鍵放開為止
+const paints$ = down$.pipe(
+    mergeMap((down) => move$.pipe(takeUntil(up$)))
+)
+
+paints$.subscribe(
+    (value) => { 
+        console.log(value)
+        paint(value)
+    },
+    (error) => {
+        console.log('Error: ', error)
+    },
+    () => { 
+        console.log('complete')
+    }
+)
